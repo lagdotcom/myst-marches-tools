74827e54367f6f28f2641164c9272e3e92d86186
Paul Davies <lagdotcom@gmail.com>
2025-09-18 16:15:33 +0100

    store PCs as json instead of string

--- api/pc.ts
+++ api/pc.ts
@@ -1,19 +1,15 @@
 import { createClient } from "redis";
 
-const notNull = (s: string | null): s is string => typeof s === "string";
+export const getRedis = () =>
+  createClient({ url: process.env.REDIS_URL }).connect();
 
 export const GET = async () => {
-  const redis = await createClient({ url: process.env.REDIS_URL }).connect();
+  const redis = await getRedis();
 
-  const keys = await redis.keys("pc:*");
-  const results = keys.length ? await redis.mGet(keys) : [];
+  const keys = await redis.KEYS("pc:*");
+  const results = keys.length ? await redis.json.MGET(keys, ".") : [];
 
-  return new Response(
-    JSON.stringify({
-      results: results.filter(notNull).map((x) => JSON.parse(x)),
-    }),
-    { status: 200 }
-  );
+  return new Response(JSON.stringify({ results }), { status: 200 });
 };
 
 export const POST = async (request: Request) => {
@@ -22,14 +18,14 @@ export const POST = async (request: Request) => {
 
   const key = `pc:${data.name}`;
 
-  const redis = await createClient({ url: process.env.REDIS_URL }).connect();
+  const redis = await getRedis();
 
-  if ((await redis.exists(key)) > 0)
+  if ((await redis.EXISTS(key)) > 0)
     return new Response(JSON.stringify({ error: "already exists" }), {
       status: 400,
     });
 
-  await redis.set(key, JSON.stringify(data));
+  await redis.json.SET(key, "$", data);
   return new Response(null, { status: 201 });
 };
 
@@ -39,13 +35,13 @@ export const PUT = async (request: Request) => {
 
   const key = `pc:${data.name}`;
 
-  const redis = await createClient({ url: process.env.REDIS_URL }).connect();
+  const redis = await getRedis();
 
-  if ((await redis.exists(key)) === 0)
+  if ((await redis.EXISTS(key)) === 0)
     return new Response(JSON.stringify({ error: "does not exist" }), {
       status: 400,
     });
 
-  await redis.set(key, JSON.stringify(data));
+  await redis.json.SET(key, "$", data);
   return new Response(null, { status: 201 });
 };
