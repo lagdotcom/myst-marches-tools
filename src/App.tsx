828da7cc949ff109e240a28bd2cc578b29df394c
Paul Davies <lagdotcom@gmail.com>
2025-10-03 16:38:12 +0100

    try SWRConfig?

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Analytics } from "@vercel/analytics/react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { SWRConfig } from "swr";
 
 import MainNav from "./components/MainNav";
 import PCPage from "./components/PCPage";
@@ -7,16 +8,24 @@ import SessionPage from "./components/SessionPage";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Analytics />
-      <MainNav />
-      <main>
-        <Routes>
-          <Route path="/pc" element={<PCPage />} />
-          <Route path="/session" element={<SessionPage />} />
-          <Route path="*" element={<Navigate to="/pc" replace />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
+    <SWRConfig
+      value={{
+        revalidateOnFocus: true,
+        revalidateOnReconnect: true,
+        shouldRetryOnError: true,
+      }}
+    >
+      <BrowserRouter>
+        <Analytics />
+        <MainNav />
+        <main>
+          <Routes>
+            <Route path="/pc" element={<PCPage />} />
+            <Route path="/session" element={<SessionPage />} />
+            <Route path="*" element={<Navigate to="/pc" replace />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
+    </SWRConfig>
   );
 }
